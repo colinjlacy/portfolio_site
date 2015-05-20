c05a6a9aebd1c64ca2e67f2702c09ad5cc83c252
colinjlacy <colin@webcake.co>
2015-05-19 19:42:37 -0500

    [app-pages] adds portfolio controller with necessary pages, styles, images, and js; adds specific routes to three pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'portfolio#index'
+
+  match 'about', to: 'portfolio#about', via: :get
+  match 'contact', to: 'portfolio#contact', via: :get
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
